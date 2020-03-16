6a8a3cc4a0535dc202b4c88d7498c1beadde9151
Sebastiaan Dekker <sebastiaan@Sebastiaans-MacBook-Pro.local>
2020-03-16 18:38:36 +0100

    Links to /event /login /signup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
-
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 export default function MyApp() {
   return (
     <React.Fragment>
       <CssBaseline />
-      {/* The rest of your application */}
       <Header></Header>
     </React.Fragment>
   );
