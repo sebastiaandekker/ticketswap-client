1f9d66ec7f7118a223824e19f0f863e7dd101999
Sebastiaan Dekker <sebastiaan@Sebastiaans-MacBook-Pro.local>
2020-03-16 20:15:00 +0100

    Styling with Material Components

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,32 +1,50 @@
 import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 
 export default class SignupForm extends React.Component {
   render() {
     return (
-      <form onSubmit={this.props.onSubmit}>
-        <input
-          type="text"
-          name="name"
-          onChange={this.props.onChange}
-          value={this.props.values.name}
-          placeholder="name"
-        />
-        <input
-          type="text"
-          name="email"
-          onChange={this.props.onChange}
-          value={this.props.values.email}
-          placeholder="email"
-        />
-        <input
-          type="text"
-          name="password"
-          onChange={this.props.onChange}
-          value={this.props.values.password}
-          placeholder="password"
-        />
-        <button type="submit">Login</button>
-      </form>
+      <Grid container justify="center">
+        <form onSubmit={this.props.onSubmit}>
+          <Grid item>
+            <TextField
+              required
+              id="standard-basic"
+              label="Name"
+              name="name"
+              onChange={this.props.onChange}
+              value={this.props.values.name}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              required
+              id="standard-basic"
+              label="Email"
+              name="email"
+              onChange={this.props.onChange}
+              value={this.props.values.email}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              required
+              id="standard-basic"
+              label="Email"
+              name="password"
+              onChange={this.props.onChange}
+              value={this.props.values.password}
+            />
+          </Grid>
+          <Grid item>
+            <Button variant="contained" color="primary" type="submit">
+              Login
+            </Button>
+          </Grid>
+        </form>
+      </Grid>
     );
   }
 }
