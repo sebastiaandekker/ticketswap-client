12443e61193aad41dab8349905ee468bcb4fb032
Sebastiaan Dekker <sebastiaan@Sebastiaans-MacBook-Pro.local>
2020-03-17 09:42:43 +0100

    LoginFormContainer componen ready for LoginForm

---
+++ src/components/LoginFormContainer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+// import SignupForm from "./SignupForm";
+import { connect } from "react-redux";
+import { signup } from "../store/user/actions";
+import { Redirect } from "react-router-dom";
+
+class LoginFormContainer extends React.Component {
+  state = { email: "", password: "" };
+
+  onSubmit = event => {
+    event.preventDefault();
+    this.props.signup(this.state.email, this.state.password);
+  };
+
+  onChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  render() {
+    if (this.props.user) {
+      return <Redirect to="/events" />;
+    }
+    return (
+      <div>
+        <h1>Login</h1>
+      </div>
+      // <SignupForm
+      //   onSubmit={this.onSubmit}
+      //   onChange={this.onChange}
+      //   values={this.state}
+      // />
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  };
+};
+
+export default connect(mapStateToProps, { signup })(LoginFormContainer);
