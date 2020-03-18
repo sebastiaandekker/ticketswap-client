b84885bde755c0df52895e73e01e913369710dc5
Sebastiaan Dekker <sebastiaan@Sebastiaans-MacBook-Pro.local>
2020-03-18 18:02:09 +0100

    Create CommentFormContainer component

---
+++ src/components/CommentFormContainer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+// import CommentForm from "./CommentForm";
+import { connect } from "react-redux";
+// import { createComment } from "../store/comment/actions";
+
+class CommentFormContainer extends React.Component {
+  state = {
+    text: ""
+  };
+
+  onSubmit = comment => {
+    comment.preventDefault();
+    this.props.createComment(this.state);
+    this.setState({
+      text: ""
+    });
+  };
+
+  onChange = comment => {
+    this.setState({
+      [comment.target.name]: comment.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div>CommentFormContainer</div>
+      // <CommentForm
+      //   onSubmit={this.onSubmit}
+      //   onChange={this.onChange}
+      //   values={this.state}
+      // />
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    user: state.user,
+    ticket: state.ticket
+  };
+};
+
+// export default connect(mapStateToProps, { createComment })(CommentFormContainer);
+export default connect(mapStateToProps)(CommentFormContainer);
--- src/components/CommentListContainer.jsx
+++ src/components/CommentListContainer.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
 import CommentList from "./CommentList";
-// import CommentFormContainer from "./CommentFormContainer";
+import CommentFormContainer from "./CommentFormContainer";
 
 class CommentListContainer extends React.Component {
   render() {
     if (!this.props.ticket) {
-      return <div>Loading Event</div>;
+      return <div>Loading Comment</div>;
     } else if (!this.props.user) {
       return <CommentList comments={this.props.ticket.comments} />;
     } else {
       return (
         <div>
-          CommentListContainer
           <CommentList comments={this.props.ticket.comments} />
-          {/* <CommentFormContainer createMode={true} /> */}
+          <CommentFormContainer />
         </div>
       );
     }
